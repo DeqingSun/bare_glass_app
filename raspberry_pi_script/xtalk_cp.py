102b6b38e1f1fc274c3cd0bf7f01283b200bb80d
Deqing Sun <iamseer@gmail.com>
2013-07-27 17:30:38 -0400

    Raspberry Pi script added
    need xmpppy libary

---
+++ raspberry_pi_script/xtalk_cp.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+# $Id: xtalk.py,v 1.4 2008/08/09 17:00:18 normanr Exp $
+import sys,os,xmpp,time,select
+
+import subprocess
+
+
+class Bot:
+
+    def __init__(self,jabber,remotejid):
+        self.jabber = jabber
+        self.remotejid = remotejid
+
+    def register_handlers(self):
+        self.jabber.RegisterHandler('message',self.xmpp_message)
+
+    def xmpp_message(self, con, event):
+        type = event.getType()
+        fromjid = event.getFrom().getStripped()
+        body = event.getBody()
+        if type in ['message', 'chat', None] and fromjid == self.remotejid and body:
+            sys.stdout.write(body + '\n')
+            self.message_analyzer(body)
+
+
+    def stdio_message(self, message):
+        m = xmpp.protocol.Message(to=self.remotejid,body=message,typ='chat')
+        self.jabber.send(m)
+
+    def xmpp_connect(self):
+        con=self.jabber.connect()
+        if not con:
+            sys.stderr.write('could not connect!\n')
+            return False
+        sys.stderr.write('connected with %s\n'%con)
+        auth=self.jabber.auth(jid.getNode(),password,resource=jid.getResource())
+        if not auth:
+            sys.stderr.write('could not authenticate!\n')
+            return False
+        sys.stderr.write('authenticated using %s\n'%auth)
+        self.register_handlers()
+        return con
+    
+    def message_analyzer(self,body):	#handle incoming messages
+        if (body.find('temperature')>=0):
+            sys.stdout.write('do sth' + '\n')
+            temperature_result=subprocess.check_output(["/opt/vc/bin/vcgencmd", "measure_temp"]).strip()
+            self.stdio_message("/push CPU temperature of Pi is: "+temperature_result[5:])
+
+    
+if __name__ == '__main__':
+
+    #if len(sys.argv) < 2:
+    #    print "Syntax: xtalk JID"
+    #    sys.exit(0)
+    
+    #tojid=sys.argv[1]
+    tojid='bareboneglass@appspot.com'
+    
+    source = 'remotedev@wtfismyip.com/pi'	#change it to your own
+    password = 'remotedev'					#change it to your own
+    target = 'bareboneglass@appspot.com'
+    
+    jid=xmpp.protocol.JID(source)
+    cl=xmpp.Client(jid.getDomain())#,debug=[])
+    
+    bot=Bot(cl,tojid)
+
+    if not bot.xmpp_connect():
+        sys.stderr.write("Could not connect to server, or password mismatch!\n")
+        sys.exit(1)
+
+    #cl.SendInitPresence(requestRoster=0)   # you may need to uncomment this for old server
+    
+    socketlist = {cl.Connection._sock:'xmpp',sys.stdin:'stdio'}
+    online = 1
+
+    while online:
+        (i , o, e) = select.select(socketlist.keys(),[],[],1)
+        for each in i:
+            if socketlist[each] == 'xmpp':
+                cl.Process(1)
+            elif socketlist[each] == 'stdio':
+                msg = sys.stdin.readline().rstrip('\r\n')
+                bot.stdio_message(msg)
+            else:
+                raise Exception("Unknown socket type: %s" % repr(socketlist[each]))
+    #cl.disconnect()
